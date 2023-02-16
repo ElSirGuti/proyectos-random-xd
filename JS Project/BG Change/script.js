f3beeaee419955282602fada699b8220f98b274d
Andres Gutierrez <87587118+ElSirGuti@users.noreply.github.com>
2023-02-16 14:25:28 -0400

    Add files via upload

---
+++ JS Project/BG Change/script.js
@@ -0,0 +1,4 @@
+var bgcolor = new Array("#029FD4", "#D43702", "#1EFF00", "#FFEA00", "AB5252", "#FFFFFF", "#DE28D8");
+
+document.body.style.background = bgcolor[Math.floor(Math.random()*bgcolor.length)];
+
