7d4c11c49f1e0a0d8da893be92a870a392eae1bd
Andres Gutierrez <87587118+ElSirGuti@users.noreply.github.com>
2023-02-17 17:37:36 -0400

    Add files via upload

--- Cars Website/script.js
+++ Cars Website/script.js
@@ -6,6 +6,14 @@ menu.onclick = () =>{
     navbar.classList.toggle('active');
 }
 
+document.querySelector('#login-btn').onclick = () =>{
+    document.querySelector('.login-form-container').classList.toggle('active');
+}
+
+document.querySelector('#close-login-form').onclick = () =>{
+    document.querySelector('.login-form-container').classList.remove('active');
+}
+
 window.onscroll = () =>{
 
     if(window.scrollY > 0){
@@ -27,3 +35,29 @@ window.onload = () =>{
     }
 
 }
+
+document.querySelector('.home').onmousemove = (e) =>{
+
+    document.querySelectorAll('.home-parallax').forEach(elm =>{
+  
+      let speed = elm.getAttribute('data-speed');
+  
+      let x = (window.innerWidth - e.pageX * speed) / 90;
+      let y = (window.innerHeight - e.pageY * speed) / 90;
+  
+      elm.style.transform = `translateX(${y}px) translateY(${x}px)`;
+  
+    });
+  
+  };
+  
+document.querySelector('.home').onmouseleave = (e) =>{
+  
+    document.querySelectorAll('.home-parallax').forEach(elm =>{
+  
+      elm.style.transform = `translateX(0px) translateY(0px)`;
+  
+    });
+  
+};
+  
