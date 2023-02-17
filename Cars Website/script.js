1b27da44e46c81225a287ea7af288f07fcbc595e
Andres Gutierrez <87587118+ElSirGuti@users.noreply.github.com>
2023-02-16 20:15:17 -0400

    Add files via upload

---
+++ Cars Website/script.js
@@ -0,0 +1,29 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+window.onload = () =>{
+
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+
+}
