46b1e31dd5293d547a7172d0bd7d2bb199885164
Andres Gutierrez <87587118+ElSirGuti@users.noreply.github.com>
2023-03-03 19:47:17 -0400

    Add files via upload

--- Cars Website/script.js
+++ Cars Website/script.js
@@ -86,4 +86,58 @@ var swiper = new Swiper(".vehicles-slider", {
         slidesPerView: 3,
       },
     },
-  });
+});
+
+var swiper = new Swiper(".featured-slider", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  grabCursor: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable:true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".reviews-slider", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  grabCursor: true,
+  centeredSlides: true,
+  autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+    },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
